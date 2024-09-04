dc07d7c5469ea5600edc52937e0ad7ef610c5bb4
Your Name <your_email@example.com>
2024-09-04 18:23:08 +0100

    changes

--- clientSide/src/components/Post.jsx
+++ clientSide/src/components/Post.jsx
@@ -128,7 +128,7 @@ const Post = ({ thought }) => {
 
   return (
     <div className="flex-1 my-8 mx-2 flex flex-col items-center w-full"
-         style={{ marginLeft: "2rem" ,  marginRight:"2rem", width: "100%" }}>
+         style={{ marginLeft: "6rem" ,  marginRight:"2rem", width: "100%" }}>
       <div
         className="my-8 mx-auto"
         style={{ maxWidth: "750px", width: "100%" }}
